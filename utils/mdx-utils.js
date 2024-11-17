6503a0b5defa062d82557245b0a15d17764ea1f2
Pedro Eugênio <peu.eugenio92@gmail.com>
2024-11-17 15:51:55 -0300

    desafio

--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -11,18 +11,10 @@ export const getPosts = async () => {
 }
 
 export const getPostById = async (id) => {
-  try {
-    const { data, error } = await api.get(`/posts?id=eq.${id}`);
+  const { data, error } = await api.get(`/posts?id=eq.${id}`);
 
-    if (error) {
-      return null;
-    }
-
-    if (data && data.length > 0) {
-      return data[0];
-    }
-    return null;
-  } catch (error) {
-    return null;
+  if(data){
+    return data;
   }
+  return {}
 }
